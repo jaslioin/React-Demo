bd4b93903abe0ec5c6d67a2af396e7eb78dffa3f
jaslioin <15674058+jaslioin@users.noreply.github.com>
2022-02-17 21:56:35 +0800

    fix: env variable not found

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-	readonly CAT_API_KEY: string;
+	readonly VITE_CAT_API_KEY: string;
 }
 
 interface ImportMeta {
--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const apiClient = axios.create({
 	baseURL: "cat-api",
 	headers: {
-		"x-api-key": import.meta.env.CAT_API_KEY,
+		"x-api-key": import.meta.env.VITE_CAT_API_KEY,
 	},
 });
