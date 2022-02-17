6cf54cb96bf686c7faeb0688a4f06a8d01dd3dcf
jaslioin <15674058+jaslioin@users.noreply.github.com>
2022-02-17 21:50:39 +0800

    chore: store api key as env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-	readonly VITE_APP_SOMETHING: string;
+	readonly CAT_API_KEY: string;
 }
 
 interface ImportMeta {
--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-    baseURL: "cat-api",
-    headers: {
-        "x-api-key": "b7945a96-706e-4075-9ecb-c677a96edf13",
-    },
-
+	baseURL: "cat-api",
+	headers: {
+		"x-api-key": import.meta.env.CAT_API_KEY,
+	},
 });
