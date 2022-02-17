e6298839b09217e29da117fc9f53ff9ac8daac5d
jaslioin <15674058+jaslioin@users.noreply.github.com>
2022-02-17 22:05:05 +0800

    fix: api proxy not working in hosting

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,7 @@
 
 interface ImportMetaEnv {
 	readonly VITE_CAT_API_KEY: string;
+	readonly VITE_CAT_API_BASE_URL: string;
 }
 
 interface ImportMeta {
--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-	baseURL: "cat-api",
+	baseURL: import.meta.env.VITE_CAT_API_BASE_URL,
 	headers: {
 		"x-api-key": import.meta.env.VITE_CAT_API_KEY,
 	},
